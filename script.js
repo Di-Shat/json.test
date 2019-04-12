9c56888f085b5c6e2179698287dd24601fa5f5fe
Dmytro Shaturnyi <shaturnyj120293@gmail.com>
2019-04-12 20:06:18 +0300

    third commit

--- script.js
+++ script.js
@@ -1,21 +1,31 @@
-let promise = new Promise(
-function(resolve,reject){
-  const request = new XMLHttpRequest()
-request.open('GET','Data.json')
+function loadData(url){
+ return new Promise(
+    function(resolve,reject){
+      const request = new XMLHttpRequest()
+      request.open('GET','Data.json')
 
-request.onreadystatechange = function(event){
-	event.target.readyState === 4 ? 
-	event.target.status === 200 ? 
-	resolve(event.target.responseText) : 
-	reject(event.target.statusText) : null
-     }
+    request.onreadystatechange = function(event){
+	  event.target.readyState === 4 ? 
+	    event.target.status === 200 ? 
+	      reject(event.target.statusText) : null
+    }
 		request.send()
    }
-)
-promise.then(
+ )
+}
+loadData('Data.json').then(
 	response => JSON.parse(response).forEach(
 			picture => document.body.appendChild(
 				document.createElement('img')).src = picture.url
 		),
 	err => console.log(err)
 )
+loadData('message.txt').then(
+	response => document.body.appendChild(
+				document.createElement('p')).innerText = response
+	)
+loadData('index.html').then(
+	response => document.body.appendChild(
+	       document.createElement('pre')).innerText = response
+	)
+	
