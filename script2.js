3fb60210f31ff95f80fb72f3a2e3249919ec18e1
Dmytro Shaturnyi <shaturnyj120293@gmail.com>
2019-05-03 17:35:28 +0300

    task-11

--- script2.js
+++ script2.js
@@ -1,18 +1,21 @@
-fetch('Data.json')
+fetch('data.json')
 	.then(
 		response => response.json()
 			.then(
-            	response => response.forEach(
-                	item => document.body.appendChild(document.createElement('img')).src = item.url
-            	)
+				response => response.forEach(
+					item => localStorage.setItem('json',JSON.stringify(item))
+				)
 			)
 	)
-	fetch('message.txt')
-		.then(
-			response => response.text()
-			.then( response =>
+fetch('message.txt')
+	.then(
+		response => response.text()
+			.then( response => 
 				document.cookie = `message=${response}`
 			)
-		)
-	document.body.appendChild(document.createElement('p')).innerText = document.cookie
+	)
+document.body.appendChild(document.createElement('p')).innerText = document.cookie
+window.addEventListener( 'storage', function(e) {  
+    console.log( 'localStorage was changed from outside' )
+})
 	
